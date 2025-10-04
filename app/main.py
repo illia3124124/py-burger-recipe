7e4bd910f69651b9ed465f5a373685ce5663bc88
Jeuxu <iliacdmn@gmail.com>
2025-10-04 20:06:40 +0300

    Solution v3

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ class Validator(ABC):
     def __set_name__(self, owner: type, name: str) -> None:
         self.protected_name = "_" + name
 
-    def __get__(self, instance: object, owner: type) -> int:
+    def __get__(self, instance: object, owner: type) -> int | Validator:
+        if instance is None:
+            return self
         return getattr(instance, self.protected_name, None)
 
     def __set__(self, instance: object, value: int) -> None:
@@ -29,14 +31,14 @@ class Number(Validator):
         if not (self.min_value <= value <= self.max_value):
             raise ValueError(f"Quantity should not be "
                              f"less than {self.min_value} "
-                             f"and greater than {self.max_value}")
+                             f"and greater than {self.max_value}.")
 
 
 class OneOf(Validator):
     def __init__(self, options: tuple) -> None:
         self.options = options
 
-    def validate(self, value: tuple) -> None:
+    def validate(self, value: str) -> None:
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}.")
 
@@ -47,7 +49,7 @@ class BurgerRecipe:
     tomatoes: int = Number(0, 3)
     cutlets: int = Number(1, 3)
     eggs: int = Number(0, 2)
-    sauce: int = OneOf(("ketchup", "mayo", "burger"))
+    sauce: str = OneOf(("ketchup", "mayo", "burger"))
 
     def __init__(self,
                  buns: int,
